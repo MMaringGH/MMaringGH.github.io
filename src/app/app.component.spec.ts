7174838243a6836316f38ecc3d5fff17604c401a
MysticalWizard <40394728+MMaringGH@users.noreply.github.com>
2021-01-18 13:59:43 +0800

    Update app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'mystical-wizard'`, () => {
+  it(`should have as title 'mysticalwizard'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mystical-wizard');
+    expect(app.title).toEqual('mysticalwizard');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('mystical-wizard app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('mysticalwizard app is running!');
   });
 });
