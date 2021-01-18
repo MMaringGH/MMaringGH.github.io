13ee55e63907d3410546dbcf516f9180451b323b
MysticalWizard <40394728+MMaringGH@users.noreply.github.com>
2021-01-18 14:01:28 +0800

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { MainComponent } from './main/main.component';
 const routes: Routes = [
   { path: '', redirectTo: 'main', pathMatch: 'full'},
   { path: 'main', component: MainComponent },
-  ];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
