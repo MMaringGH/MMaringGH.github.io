15a8c8878fb10962f2410b1b5fc999d32943eb82
MysticalWizard <40394728+MMaringGH@users.noreply.github.com>
2021-02-02 00:02:46 +0800

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { MainComponent } from './main/main.component';
-import { HomeComponent } from './main/home/home.component';
-import { AboutComponent } from './main/about/about.component';
-import { SocialComponent } from './main/social/social.component';
-import { ProjectsComponent } from './main/projects/projects.component';
+import { HomeComponent } from './main/home.component';
+import { AboutComponent } from './main/about.component';
+import { SocialComponent } from './main/social.component';
+import { ProjectsComponent } from './main/projects.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'main/home', pathMatch: 'full'},
-  { path: '*', redirectTo: 'main/home', pathMatch: 'full'},
-  { path: 'main',
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: '*', redirectTo: 'home',  pathMatch: 'full'},
+  { path: '',
     component: MainComponent,
     children: [
       { path: 'home', component: HomeComponent },
