bf00a19f7744cc5f37086a06a1eed1ced1833a1e
MysticalWizard <mmaring05@naver.com>
2021-06-26 19:09:53 +0900

    Add RouterModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -69,6 +70,7 @@ import { CovalentHttpModule } from '@covalent/http';
   imports: [
     BrowserModule,
     HttpClientModule,
+    RouterModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatAutocompleteModule,
