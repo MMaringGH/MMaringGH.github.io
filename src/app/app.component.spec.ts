11b7fc98b0014975abe0d4d2d0b818170cea0512
MysticalWizard <mmaring05@naver.com>
2021-06-26 19:09:42 +0900

    Update Tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('mysticalwizard');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('mysticalwizard app is running!');
-  });
 });
